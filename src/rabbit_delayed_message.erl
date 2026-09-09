9ad067768ab07277dc16d1abeb45ade4e2490394
Lois Soto Lopez <lois@84codes.com>
2026-09-09 11:10:03 +0200

    Re-label operator exports

--- src/rabbit_delayed_message.erl
+++ src/rabbit_delayed_message.erl
@@ -29,7 +29,7 @@
 -export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2,
          code_change/3]).
 
-%% Testing & debugging exports
+%% Operator exports
 -export([refresh_config/0,
          compact_journal/0]).
 
